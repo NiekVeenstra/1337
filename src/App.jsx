c55ae4c32153c58bc13921c395f2b1df8c699244
Niek <n.veenstra02@gmail.com>
2021-09-10 16:01:26 +0200

    setting up navbar + dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import DropdownMenu from "./components/navbar/DropdownMenu";
+import Navbar from "./components/navbar/Navbar";
+import NavItem from "./components/navbar/NavItem";
 
 const App = () => {
   return (
     <div>
-      <h1>app</h1>
+      <Navbar>
+        <NavItem location="Home" />
+        <NavItem location="Mint" />
+        <NavItem location="About" />
+
+        <NavItem location="test">
+          <DropdownMenu />
+        </NavItem>
+      </Navbar>
     </div>
   );
 };
