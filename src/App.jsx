bf5d15ea0783561a09e0ad23dc9e6fb52edcedb4
Niek <n.veenstra02@gmail.com>
2021-09-15 19:38:25 +0200

    adding leet text

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import DropdownMenu from "./components/navbar/DropdownMenu";
 import Navbar from "./components/navbar/Navbar";
 import NavItem from "./components/navbar/NavItem";
 import { Context } from "./Context/Context";
+import Home from "./pages/Home";
 
 const App = () => {
   const [activeMenu, setActiveMenu] = useState("main");
 
-
-
   return (
     <div>
-      <Context.Provider value={{activeMenu, setActiveMenu}}>
+      <Context.Provider value={{ activeMenu, setActiveMenu }}>
         <Navbar>
           <NavItem location="H0m3" />
           <NavItem location="M1n7" />
           <NavItem location="4b0u7" />
         </Navbar>
+        <Home />
       </Context.Provider>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="home">
+            <h2 className="home__introText">
+                <span>w3lc0m3 3l3375</span>
+                <span>7h15 6r0up 15 0nly f0r 1337 5up3r10r b31n65</span>
+                <span>74k3 4 l34p 0f f417h 4nd b3c0m3 p4r7 0f 7h3 3l337 3n717y</span>
+            </h2>
         </div>
     )
 }
