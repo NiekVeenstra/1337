214bd41c74fe5e22a9ed4aaee4eac37eea5b3088
Niek <n.veenstra02@gmail.com>
2021-09-20 14:06:32 +0200

    create mint page

--- src/pages/Mintpage.jsx
+++ src/pages/Mintpage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Mintpage = () => {
-    return (
-        <div className="mintpage">
-            
-        </div>
-    )
-}
+  return (
+    <div className="mintpage">
+      <h2 className="mintpage__title">M1N7</h2>
 
-export default Mintpage
+      <div className="mintpageOutercontainer">
+        <div className="mintContainer">
+          <h3></h3>
+        </div>
+        <div className="mintImage"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Mintpage;
